ce9e809feb3ac3b50c3a04378989d7be89b42a0e
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-10-29 12:57:00 +0000

    feat: use pretty_print_json

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from geolocate import select_geolocation_method, geolocate
 from weather_data import *
 from weather_data import *
+from pretty_print_json import *
 
 async def main():
     geolocation_method = select_geolocation_method()
     geolocation = await geolocate(geolocation_method)
-    data = await get_weather_data(geolocation)
-    
-    print(data) 
+    weather_data = await get_weather_data(geolocation)
+    pretty_print_json(weather_data)
+    exit()
 
 if __name__ == "__main__":
     import asyncio
