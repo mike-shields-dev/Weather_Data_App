4173f20f3e85cab9e40f8b384feb88e253ed794d
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-12-15 18:55:11 +0000

    refactor: Code splitting import get_user_selection

--- geolocate.py
+++ geolocate.py
@@ -1,34 +1,29 @@
 import os
 import geocoder
-import inquirer
 import httpx
 from dotenv import load_dotenv
 
-load_dotenv()
+from get_user_selection import *
 
+# intentionally global
 geolocation_method_options = ["From my IP address", "Provide place name"]
 
 
-def get_user_geolocation_method_selection():
-    question = [
-        inquirer.List(
-            "geolocation_method",
-            message="What geolocation method would you like to use to get weather data?",
-            choices=geolocation_method_options,
-        ),
-    ]
+async def get_user_geolocation_method_selection():
+    selected_geolocation_method = await get_user_selection(
+        topic="geolocation_selection",
+        message="What method would you like to use to get Weather Data?",
+        choices=geolocation_method_options,
+    )
 
-    choices = inquirer.prompt(question)
-    locate_method = choices["geolocation_method"]
-
-    return locate_method
+    return selected_geolocation_method
 
 
 async def get_coordinates(geolocation_method):
     if geolocation_method == geolocation_method_options[0]:
         return get_coordinates_from_ip_address()
     elif geolocation_method == geolocation_method_options[1]:
-        return await get_coords_from_place_name()
+        return await get_coordinates_from_place_name()
     else:
         return get_coordinates_from_ip_address()
 
@@ -39,66 +34,59 @@ def get_coordinates_from_ip_address():
     return g.latlng
 
 
-async def get_coords_from_place_name():
-    place = await get_desired_place()
+async def get_coordinates_from_place_name():
+    place = await get_user_selected_place_data()
 
     return [place["lat"], place["lon"]]
 
 
-def get_user_place_selection(places):
-    options = [
-        f"{place['name']}, {place['region']}, {place['country']}" for place in places
-    ]
+def format_place(place):
+    return f"{place['name']}, {place['region']}, {place['country']}"
 
-    question = [
-        inquirer.List(
-            "selected_location",
-            message="Multiple matches found, please choose one",
-            choices=options,
-        )
-    ]
+async def get_user_place_selection(places):
+    formatted_places = {format_place(place): place for place in places}
+    places_options = list(formatted_places.keys())
 
-    answers = inquirer.prompt(question)
-    selected_location = answers["selected_location"]
+    selected_place = await get_user_selection(
+        topic="selected_place",
+        message="Multiple matches found, please choose one",
+        choices=places_options,
+    )
 
-    for place in places:
-        if (
-            selected_location
-            == f"{place['name']}, {place['region']}, {place['country']}"
-        ):
-            return place
+    return formatted_places.get(selected_place)
 
 
-async def get_desired_place():
+async def fetch_places(place_name, headers, SEARCH_URL):
+    async with httpx.AsyncClient() as client:
+        try:
+            response = await client.get(
+                SEARCH_URL, headers=headers, params={"q": place_name}
+            )
+            return response.json()
+        except httpx.RequestError as e:
+            print(f"Error fetching data, please try again: {str(e)}")
+            return []
+
+async def get_user_selected_place_data():
+    load_dotenv()
+    
     WEATHER_API_KEY = os.getenv("WEATHER_API_KEY")
     WEATHER_API_HOST = os.getenv("WEATHER_API_HOST")
-    BASE_URL = "https://" + WEATHER_API_HOST
-    SEARCH_URL = BASE_URL + "/search.json"
+    BASE_URL = f"https://{WEATHER_API_HOST}"
+    SEARCH_URL = f"{BASE_URL}/search.json"
 
     headers = {
         "X-RapidAPI-Key": WEATHER_API_KEY,
         "X-RapidAPI-Host": WEATHER_API_HOST,
     }
 
-    place_name = input("Please enter place name:\n")
+    while True:
+        place_name = input("Please enter place name:\n")
+        places = await fetch_places(place_name, headers, SEARCH_URL)
 
-    async with httpx.AsyncClient() as client:
-        while True:
-            try:
-                response = await client.get(
-                    SEARCH_URL, headers=headers, params={"q": place_name}
-                )
-
-                matching_place_names = response.json()
-
-                if len(matching_place_names) == 0:
-                    place_name = input(
-                        "No matches found, please try entering place name again \n"
-                    )
-                if len(matching_place_names) == 1:
-                    return matching_place_names[0]
-                if len(matching_place_names) > 1:
-                    return get_user_place_selection(matching_place_names)
-
-            except httpx.RequestError as e:
-                print(f"Error fetching data, please try again: {str(e)}")
+        if len(places) == 0:
+            print("No matches found, please try entering place name again.")
+        elif len(places) == 1:
+            return places[0]
+        else:
+            return await get_user_place_selection(places)
