78ccd8b00a391dfdf850c63ace9c67e87074d515
mike-shields-dev <mr.m.p.shields@gmail.com>
2023-12-15 18:55:47 +0000

    refactor: Tidy up main.py

--- main.py
+++ main.py
@@ -1,17 +1,14 @@
 from geolocate import get_user_geolocation_method_selection, get_coordinates
 from weather_data import *
 from pretty_print_json import *
-
+import asyncio
 
 async def main():
-    selected_geolocation_method = get_user_geolocation_method_selection()
+    selected_geolocation_method = await get_user_geolocation_method_selection()
     coordinates = await get_coordinates(selected_geolocation_method)
     weather_data = await get_weather_data(coordinates)
     pretty_print_json(weather_data)
+    
     exit()
 
-
-if __name__ == "__main__":
-    import asyncio
-
-    asyncio.run(main())
+asyncio.run(main())
